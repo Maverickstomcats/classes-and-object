fa01a83f9231615b099edbaa5dd82602f73f1062
tunatuna tunatuna <113930372+Maverickstomcats@users.noreply.github.com>
2023-03-20 21:31:26 +0000

    stuff

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 #create a house class
+class anime:
+  #constructor method are  called methods
+  #variables in classes are attributes:
+  def __init__(self,area,windows,width,height,colors,floors,occupied):
+    self.area = area
+    self.windows = windows
+    self.width = width
+    self.height = height
+    self.colors = colors
+    self.floors = floors
+    self.occupied = occupied 
+
+def area(width,height):
+  return width * height
